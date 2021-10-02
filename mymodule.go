bed075015fc8b70f3e2d3215e3bce02af3ec49c1
JustAProgrammer01234 <scriptoentity@gmail.com>
2021-10-02 16:15:38 +0800

    Added some funcs and stuff.

---
+++ mymodule.go
@@ -0,0 +1,24 @@
+package mymodule
+
+import "fmt"
+
+func hello(name string) string {
+	return "Hello there!"
+}
+
+func add(addend1 int, addend2 int) int {
+	return addend1 + addend2
+}
+
+func greet(name string) string {
+	greeting := fmt.Sprintf("Hello %v, nice to see you!", name)
+	return greeting
+}
+
+func iseven(num int) bool {
+	if num%2 == 0 {
+		return true
+	} else {
+		return false
+	}
+}
